ff0dfcef2f893d6a38b2178a7b8ab2edf7532cb4
Raphaël Pinel <raphaelpinel@gmail.com>
2020-04-22 11:41:08 +0300

    isolate loading indicator on only one delete buton

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
 import { Container, Loader } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
@@ -12,6 +12,7 @@ const App = () => {
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
+  const [target, setTarget] = useState('');
 
   const handleSelectActivity = (id: string | null) => {
     console.log('id in handleSelectActivity:', id);
@@ -46,8 +47,9 @@ const App = () => {
       .then(() => setSubmitting(false));
   };
 
-  const handleDeleteActivity = (id: string) => {
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
     setSubmitting(true);
+    setTarget(event.currentTarget.name);
     agent.Activities.delete(id)
       .then(() => {
         setActivities([...activities.filter((a) => a.id !== id)]);
@@ -85,6 +87,7 @@ const App = () => {
           editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
+          target={target}
         />
       </Container>
     </Fragment>
