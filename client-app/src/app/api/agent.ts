e4279fdc93fe3c9475ffc5688c4f0f97555a0683
Raphaël Pinel <raphaelpinel@gmail.com>
2020-04-22 10:28:09 +0300

    create agent file for mapping axios to api

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse } from 'axios';
+import { IActivity } from '../models/activity';
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url).then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Activities = {
+  list: (): Promise<IActivity[]> => requests.get('/activities'),
+  details: (id: string): Promise<IActivity> => requests.get(`/activities/${id}`),
+  create: (activity: IActivity): Promise<void> => requests.post('/activities', activity),
+  update: (activity: IActivity): Promise<void> => requests.put('/activities', activity),
+  delete: (id: string): Promise<void> => requests.del(`/activities/${id}`),
+};
+
+export default {
+  Activities,
+};
