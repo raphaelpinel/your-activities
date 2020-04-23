4c1c61769e0bc203a6aa6e4ecb1c997351d1c7fe
Raphaël Pinel <raphaelpinel@gmail.com>
2020-04-23 11:40:21 +0300

    add react router

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../app/stores/activityStore';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const activityStore = useContext(ActivityStore);
@@ -9,13 +10,13 @@ const NavBar = () => {
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item>
+        <Menu.Item header as={NavLink} exact to="/">
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
           Your Activities
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item name="Activities" as={NavLink} to="/activities" />
         <Menu.Item>
-          <Button onClick={openCreateForm} positive content="Create Activity" />
+          <Button as={NavLink} to="/create-activity" onClick={openCreateForm} positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
